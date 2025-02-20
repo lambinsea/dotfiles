9bf3c7ecae156e07fb7978733b5aacd62108885b
lambinsea <lambinsea@protonmail.com>
2025-02-19 23:51:41 -0800

    update confs

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+# important commands
+alias eww="/home/lambinsea/.installation/eww/target/release/eww"
+
+# fun
+alias hello="echo "hello!""
