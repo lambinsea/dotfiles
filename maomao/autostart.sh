f2c9eefb27c0267fcf82300f66c606c20803a6d5
lambinsea@protonmail.com <lambinsea@protonmail.com>
2025-06-23 01:19:16 -0700

    init

---
+++ maomao/autostart.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/fish
+
+# notifications
+swaync &
+
+# night light
+
+# wallpaper
+# swaybg -i ~/.config/maomao/wallpaper/
+
+# bar
+waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css &
+
+nm-applet & fcitx5 &
+
+# clipboard
+wl-clip-persist --clipboard regular --reconnect-tries 0 &
+wl-paste --type text --watch cliphist store &
+
+# permissions
+
+/usr/lib/xfce-polkit/xfce-polkit &
