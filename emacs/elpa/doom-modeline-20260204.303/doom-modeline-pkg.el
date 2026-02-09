04a67f7f604bbb0f076341f8bdc15c0724d2cf78
lambinsea <lambinsea@protonmail.com>
2026-02-09 11:56:18 -0800

    add initial emacs config

---
+++ emacs/elpa/doom-modeline-20260204.303/doom-modeline-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "doom-modeline" "20260204.303"
+  "A minimal and modern mode-line."
+  '((emacs       "25.1")
+    (compat      "30.1.0.0")
+    (nerd-icons  "0.1.0")
+    (shrink-path "0.3.1"))
+  :url "https://github.com/seagle0128/doom-modeline"
+  :commit "0cd7797eca092b7800da77547471cae2011a2fd2"
+  :revdesc "0cd7797eca09"
+  :keywords '("faces" "mode-line")
+  :authors '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers '(("Vincent Zhang" . "seagle0128@gmail.com")))
---
+++ emacs/init.el
@@ -0,0 +1,22 @@
+;; Set up a minimal config
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+
+(menu-bar-mode -1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(command-log-mode counsel doom-modeline)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
